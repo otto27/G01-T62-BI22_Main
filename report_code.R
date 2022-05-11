53b94ba650b139630d4b0d9ead1e92ee0a1fd46a
Carlos <carlos.develop@outlook.com>
2022-05-11 17:46:32 +0100

    Aula 11 maio. Factors/Criacao do file sales_av.Rda

--- report_code.R
+++ report_code.R
@@ -34,7 +34,7 @@ for(n in nomes_folhas) {
     assign(n, read_excel("bidm.xlsx", sheet = n))
     save(list = n, file = paste0(n, ".Rda"))
 }
-remove(n, nomes_folhas)
+#remove(n, nomes_folhas)
 # -------------------------------------------------
 
 
@@ -46,4 +46,31 @@ remove(n, nomes_folhas)
 str(customers)
 dim(customers)
 
+#O calendário não é preciso considerar, pois existem funções que permitem extrair os dados
+
+#carregar todos os dataframes para RAM
+
+file_nomes <- list.files(getwd(), pattern = ".Rda")
+dont_load <- c("calendar", "productCategory", "productSubCategory")
+
+for (i in file_nomes) {
+    load(i)
+}
+rm(i, file_nomes)
+
 # -------------------------------------------------
+
+#Feito na aula 11 maio
+
+factSales$salesYear <- as.factor(year(factSales$OrderDate))
+factSales$salesQuarter <- as.factor(quarters(factSales$OrderDate))
+factSales$salesMonth <- as.factor(months(factSales$OrderDate, abbreviate = TRUE))
+factSales$salesDay <- as.factor(weekdays(factSales$OrderDate, abbreviate = TRUE))
+
+factSales$salesMonth <- ordered(factSales$salesMonth, 
+                                levels = c("january", "february", "march", 
+                                           "april", "may", "june", "july",
+                                           "august", "september", "october",
+                                           "november", "december"))
+
+
