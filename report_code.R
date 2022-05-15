61a72d74b8ce3a455237aee210c9fb57ccc42823
Carlos <carlos.develop@outlook.com>
2022-05-15 12:44:03 +0100

    Alteracoes feitas no report code para saber quantidades vendidas

--- report_code.R
+++ report_code.R
@@ -1,11 +1,14 @@
 #### -----
 #### 
-#### código para o relatório
+#### Código para o relatório
 ####
 #### -----
 
 
-## instalação de bibliotecas ##
+## ************ ROUTINES NEEEDED SO THE CODE CAN RUN SMOOTHLY *****************
+## Run them at the start of every session
+
+## Instalação de bibliotecas ##
 ## executar em todas as sessões ##
 
 bibliotecas <- c("knitr", "rmdformats", "readxl", "lubridate", "ggplot2", "DT",
@@ -20,13 +23,12 @@ for (bibl in bibliotecas) {
     }
 }
 rm(bibl, bibliotecas)
-# ----------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 
 
-
-## leitura dos dados e construção de data frames ##
-## executar apenas uma vez! ##
+## Leitura dos dados e construção de data frames ##
+## Executar apenas uma vez! ##
 
 library("readxl")
 nomes_folhas <- excel_sheets("bidm.xlsx")
@@ -34,34 +36,49 @@ for(n in nomes_folhas) {
     assign(n, read_excel("bidm.xlsx", sheet = n))
     save(list = n, file = paste0(n, ".Rda"))
 }
-#remove(n, nomes_folhas)
-# -------------------------------------------------
+remove(n, nomes_folhas)
+# ------------------------------------------------------------------------------
+
+## *************        UTILS  
+## Run in the beginning of every session
+
+
+# Negate the %in% operator - nin significa Not In 
+# https://www.rdocumentation.org/packages/functional/versions/0.6/topics/Negate
+`%nin%` = Negate(`%in%`)
+
+## ************             END OF ROUTINES                   *****************
 
 
 
+# -------------          -------------------          ----------------   ------
 
 
-## operações com dataframes ##
-##  dá a estrutura e dimensão da sheet customers ##
+
+## *****************  DATA FRAME MANIPULATION **********************************
+##estrutura e dimensão da sheet customers ##
 str(customers)
 dim(customers)
 
-#O calendário não é preciso considerar, pois existem funções que permitem extrair os dados
 
-#carregar todos os dataframes para RAM
+###  Carregar todos os dataframes para RAM
 
 file_nomes <- list.files(getwd(), pattern = ".Rda")
-dont_load <- c("calendar", "productCategory", "productSubCategory")
+dont_load <- c("calendar.Rda", "productCategory.Rda", "productSubCategory.Rda")
+#O calendário não é preciso considerar, pois existem funções que permitem 
+#próprias do R para esse efeito
 
 for (i in file_nomes) {
-    load(i)
+    if (i %nin% dont_load) {
+        load(i)
+    }
 }
 rm(i, file_nomes)
 
-# -------------------------------------------------
+# -----------------------------------------------------------------------------
 
 #Feito na aula 11 maio
-
+#Determinação dos dados categorizados como Factor
 factSales$salesYear <- as.factor(year(factSales$OrderDate))
 factSales$salesQuarter <- as.factor(quarters(factSales$OrderDate))
 factSales$salesMonth <- as.factor(months(factSales$OrderDate, abbreviate = TRUE))
@@ -72,5 +89,32 @@ factSales$salesMonth <- ordered(factSales$salesMonth,
                                            "april", "may", "june", "july",
                                            "august", "september", "october",
                                            "november", "december"))
+
+
+## SALES PRODUCT ANALYSIS 
+
+# Generate leaner dataframes
+sales_product <- merge(factSales, products, by = "ProductKey")
+sales_product <- subset(sales_product, select = -c(SalesOrderLineNumber))
+save(sales_product, file = "sales_product.Rda")
+
+
+##QUE CATEGORIA DE PRODUTO VENDE MAIS - quantity?
+#By Product Category
+#https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/table
+sales_category_totals <- table(sales_product$Category)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
